45e4398984077fb8eb64aee0d193ff2afec47b64
Faeq <61658458+Faeq-F@users.noreply.github.com>
2022-04-25 11:53:11 +0100

    Add files via upload

--- assets/Python scripts/unicode.py
+++ assets/Python scripts/unicode.py
@@ -1,17 +1,28 @@
-import unicodedata 
+import unicodedata
 import sys
 import json
+#import os
 allItems = []
+#function to find chars
 def UnicodeLookup(char):
-    for i in range(0x110000): 
-        character = chr(i) 
+    #for every Unicode character in the range of usable characters
+    for i in range(0x110000):
+        #character wanted
+        character = chr(i)
+        #name of character (description of what it is)
         name = unicodedata.name(character, "")
-        if ' '+char.upper() in f"{name}":
-            item = [f"{character}",f"{name}"]
+        #if character wanted
+        if ''+char.upper() in f"{name}":
+            #create item object
+            item = {"Icon": "char", "Title": f"{character}", "Subtitle": f"{name}"}
+            #append to all array
             allItems.append(item)
-            json_string = json.dumps(allItems)
-            f = open("chars.json", "w")
-            f.write(json_string)
-            f.close()
-
+    #convert to JSON
+    json_string = json.dumps(allItems)
+    #jsonDumpPath = os.getcwd()[:-14]+"\JSON data\chars.json"
+    #with open(jsonDumpPath, 'w') as file:
+    #   file.write(json_string)
+    #output JSON
+    print(json_string)
+#running function with argument
 UnicodeLookup(sys.argv[1])
