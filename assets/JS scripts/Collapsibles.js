bf9b1b0fc321ca43489e583e0f81f2400d911336
Faeq <61658458+Faeq-F@users.noreply.github.com>
2022-12-31 13:54:01 +0000

    Delete Collapsibles.js

--- assets/JS scripts/Collapsibles.js
+++
@@ -1,9 +0,0 @@
-//runs when settings window is loaded
-document.addEventListener("DOMContentLoaded", function () {
-  //gets all collapsibles
-  var elem = document.querySelectorAll(".collapsible");
-  //initialises all collapsibles
-  var instance = M.Collapsible.init(elem, {
-    accordion: false,
-  });
-});
