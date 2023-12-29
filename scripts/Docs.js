d8c7310dcde332363215e0c30182d7d51213d7be
Faeq <faeqfaisal@hotmail.co.uk>
2023-12-29 17:35:34 +0000

    added functionality to Documentation section of page

--- scripts/Docs.js
+++ scripts/Docs.js
@@ -81,3 +81,33 @@ function toggleFillPage() {
 }
 
 document.getElementById("FillPageButton").onclick = toggleFillPage;
+
+/*Buttons*/
+var buttons = document.querySelectorAll("#DocsNav > ul > li > button");
+var buttons_array = [...buttons]; // converts NodeList to Array
+
+function loadDocsContent(buttonText) {
+  var button = buttonText.replace(/ /g, "_");
+  $("#DocsContent").load("../_layouts/" + button + "Cards.html");
+}
+function attachButtonListeners() {
+  buttons_array.forEach(function (item, idx) {
+    item.addEventListener("click", function () {
+      loadDocsContent(item.innerText);
+    });
+  });
+}
+
+attachButtonListeners();
+
+var buttons = document.querySelectorAll("#DocsNav > ul > ul > li > button");
+var buttons_array = [...buttons]; // converts NodeList to Array
+attachButtonListeners();
+
+function loadDefaultDocsContent() {
+  $("#DocsContent").load("../_layouts/defaultCards.html");
+}
+
+document.getElementById("DocsHeaderButton").onclick = loadDefaultDocsContent;
+
+loadDefaultDocsContent();
