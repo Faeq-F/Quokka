4a4b911896beb324cb358e712cc5c812b1d86690
Faeq <faeqfaisal@hotmail.co.uk>
2024-07-07 19:09:15 +0100

    changing styles for documentation & cleaning up files

--- scripts/Docs.js
+++ scripts/Docs.js
@@ -1,9 +1,10 @@
 /*Buttons*/
-var buttons = document.querySelectorAll("#DocsNav > ul > li > button");
+var buttons = document.querySelectorAll("#DocsNav > ul > li > a");
 var buttons_array = [...buttons]; // converts NodeList to Array
 
 function loadDocsContent(buttonText) {
   var button = buttonText.replace(/ /g, "_");
+  button = button.replace("⤷", "");
   $("#DocsContent").load("DocumentationCards/" + button + ".html");
 }
 
@@ -11,12 +12,21 @@ function attachButtonListeners() {
   buttons_array.forEach(function (item, idx) {
     item.addEventListener("click", function () {
       loadDocsContent(item.innerText);
+      var oldActive = document.getElementsByClassName("active")[0];
+      if (oldActive) {
+        oldActive.classList.remove("active");
+        oldActive
+          .getElementsByTagName("svg")[0]
+          .classList.remove("app-color-primary");
+      }
+      item.classList.add("active");
+      item.getElementsByTagName("svg")[0].classList.add("app-color-primary");
     });
   });
 }
 attachButtonListeners();
 
-var buttons = document.querySelectorAll("#DocsNav > ul > ul > li > button");
+var buttons = document.querySelectorAll(".app-navbar-submenu-content > li > a");
 var buttons_array = [...buttons]; // converts NodeList to Array
 attachButtonListeners();
 
--- scripts/Gallery.js
+++ scripts/Gallery.js
@@ -1,4 +1,3 @@
-/*Buttons*/
 var buttons = document.querySelectorAll(".ProjectCard");
 var buttons_array = [...buttons]; // converts NodeList to Array
 
@@ -17,42 +16,9 @@ function attachButtonListeners() {
 }
 attachButtonListeners();
 
-function loadDefaultPluginsScreenshots() {
-  $("#PluginScreenshotsGrid").load("PluginScreenshots/InitialScreenshots.html");
-}
-document.getElementsByClassName("PortfolioWorkHeader")[0].onclick =
-  loadDefaultPluginsScreenshots;
-loadDefaultPluginsScreenshots();
+$("#PluginScreenshotsGrid").load("PluginScreenshots/InitialScreenshots.html");
 
 const vw = Math.max(
   document.documentElement.clientWidth || 0,
   window.innerWidth || 0
 );
-
-var modalWindow = document.getElementsByClassName("modal-window");
-var modalOverlay = document.getElementsByClassName("modal-overlay")[0];
-
-var images = document.getElementsByClassName("modal-launcher");
-function exitImageView() {
-  for (i in images) images[i].checked = false;
-  if (vw > 660) $("header").css("display", "block");
-  modalWindow.removeClass("fadeIn");
-  modalOverlay.removeClass("fadeIn");
-  modalWindow.addClass("fadeOut");
-  modalOverlay.addClass("fadeOut");
-}
-
-// $($window).addEventListener("click", function () {
-//   exitImageView();
-// });
-// $($overlay).addEventListener("click", function () {
-//   exitImageView();
-// });
-
-$(".modal-launcher").click(function () {
-  if (vw > 660) $("header").css("display", "none");
-  modalWindow.removeClass("fadeOut");
-  modalOverlay.removeClass("fadeOut");
-  modalWindow.addClass("fadeIn");
-  modalOverlay.addClass("fadeIn");
-});
