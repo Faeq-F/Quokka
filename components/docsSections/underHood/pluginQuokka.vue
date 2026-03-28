f80ed5af778e1a8046205039e0ba448bb01f9c51
Faeq <faeqfaisal@hotmail.co.uk>
2026-03-28 20:17:42 +0000

    docs(underTheHood): :memo: The Quokka Plugin

---
+++ components/docsSections/underHood/pluginQuokka.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ExternalLink from '~/components/link.vue'
+import Code from '~/components/code.vue'
+import DocNotice from '~/components/docNotice.vue'
+</script>
+<template>
+  <div class="p-10">
+    Quokka's built-in utility plugin.
+    <DocNotice type="info" class="mt-4" style="width: calc(100% - 1.5rem)">
+      Contains the following functionality:
+      <ul>
+        <li>
+          Checking for updates manually & on startup
+        </li>
+        <li>
+          Watching the settings file for changes to suggest a restart of the
+          application
+        </li>
+        <li>
+          Provides a view for licenses of software used by Quokka
+        </li>
+        <li>
+          Provides useful links about Quokka
+        </li>
+      </ul>
+    </DocNotice>
+  </div>
+</template>
