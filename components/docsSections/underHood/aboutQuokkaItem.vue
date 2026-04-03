f421d3111fb79ed2247eb75a822de8b79856eeee
Faeq <faeqfaisal@hotmail.co.uk>
2026-04-03 17:41:04 +0100

    docs(underTheHood): :memo: aboutQuokkaItem

---
+++ components/docsSections/underHood/aboutQuokkaItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ExternalLink from '~/components/link.vue'
+import Code from '~/components/code.vue'
+import DocNotice from '~/components/docNotice.vue'
+</script>
+<template>
+  <div class="p-10">
+    The list item that appears when the user has entered the
+    <ExternalLink url="/Quokka/documentation?section=settings">
+      <Code inline>&nbsp;AboutCommand&nbsp;</Code>
+    </ExternalLink>
+    special command.
+  </div>
+</template>
